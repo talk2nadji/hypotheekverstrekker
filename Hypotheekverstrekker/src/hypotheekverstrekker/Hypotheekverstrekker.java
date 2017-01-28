43a6c099977f230dea48c2dd9df4b8c7bdd9a7eb
talk2nadji <talk2nadji@hotmail.com>
2017-01-28 11:46:01 +0100

    initialize Hypotheekverstrekker

---
+++ Hypotheekverstrekker/src/hypotheekverstrekker/Hypotheekverstrekker.java
@@ -0,0 +1,14 @@
+
+package hypotheekverstrekker;
+
+/**
+ *
+ * @author Raoef Nadji
+ */
+public class Hypotheekverstrekker {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Hypotheekverstrekker/src/hypotheekverstrekker/Product.java
@@ -0,0 +1,7 @@
+
+package hypotheekverstrekker;
+
+
+public interface Product {
+    
+}
